783e68e6cd3c49a0f56f692f9e81d656ad2bf388
Vinicius Silveira <vinicius.silveirag@outlook.com>
2025-03-17 20:47:45 -0300

    Estudos dia 17.03

---
+++ tipos-variaveis/src/App.java
@@ -0,0 +1,11 @@
+public class App {
+    public static void main(String[] args) throws Exception {
+            byte idade = 123;
+            short ano = 2021;
+            int cep = 21070333;
+            long cpf = 98765432109L;
+            float pi = 3.14F;
+            double salario = 1275.33;
+
+    }
+}
