091a861aa388130454a88b8217c649cb6a807e9b
Vinicius Silveira <vinicius.silveirag@outlook.com>
2025-03-18 18:28:11 -0300

    Adiciona classe SmartTv e Usuario

--- Operadores/Operadores/src/Operadores.java
+++ Operadores/Operadores/src/Operadores.java
@@ -1,6 +1,8 @@
 public class Operadores {
     public static void main(String[] args) throws Exception {
-
+            public double somar (int num1, int num2) {
+                
+            }
         
 
 
